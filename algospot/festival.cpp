d7b99805200226c6da93b9fa6a4e4e53cee945ca
Woongbinni <woongbinni@gmail.com>
2015-11-01 23:22:46 +0900

    algospot FESTIVAL solved.

--- algospot/festival.cpp
+++ algospot/festival.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -8,23 +7,38 @@ int main(void){
   int N, L;
   int cost;
   int day[1005];
-  int i, j, k;
+  int i, j, k, l;
+  double sum;
+  double temp;
+  double result;
 
   scanf("%d", &test_case);
   for(i=0; i<test_case; i++){
+    result = 99999;
+    sum = 0;
+    memset(day, 0x00, sizeof(day));
     scanf("%d%d", &N, &L);
+
     for(j=0; j<N; j++){
       scanf("%d", &cost);
       day[j] = cost;
     }
 
-    for(j=0; j<N; j++){
-      printf("%d ", day[j]);
+    for(j=L; j<=N; j++){
+      for(k=0; k<=N-j; k++){
+        sum = 0;
+        for(l=k; l<k+j; l++){
+          sum += day[l];
+        }
+        temp = (double)sum/j;
+        if(temp < result){
+          result = temp;
+        }
+      }
     }
-    printf("\n");
+
+    printf("%.11f\n", result);
   }
-
-
 
   return 0;
 }
