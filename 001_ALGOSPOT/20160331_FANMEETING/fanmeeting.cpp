438272162bc32fa80ff0346fd926e5c414fff7be
Woongbinni <woongbinni@gmail.com>
2016-04-01 01:09:25 +0900

    fanmeeting cpp버전으로 코딩

---
+++ 001_ALGOSPOT/20160331_FANMEETING/fanmeeting.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void){
+    int test_case;
+
+    scanf("%d", &test_case);
+
+    for(int i=0; i<test_case; ++i){
+        char members[200001];
+        char fans[200001];
+        scanf("%s", members);
+        scanf("%s", fans);
+
+        int len_members = strlen(members);
+        for(int j=0; j<len_members; j++){
+            if(members[j] == 'M')
+                members[j] -= 'L';
+            else
+                members[j] -= 'F';
+        }
+
+        int len_fans = strlen(fans);
+        for(int j=0; j<len_fans; j++){
+            if(fans[j] == 'M')
+                fans[j] -= 'L';
+            else
+                fans[j] -= 'F';
+        }
+
+        int len_until = len_fans - len_members + 1;
+        int hug_cnt = 0;
+        for(int j=0; j<len_until; j++){
+            int sum = 0;
+            for(int k=0; k<len_members; k++){
+                sum += (members[k] && fans[j+k]);
+                if(sum > 0)
+                    break;
+            }
+            if(sum == 0)
+                hug_cnt += 1;
+        }
+
+        printf("%d\n", hug_cnt);
+    }
+
+    return 0;
+}
