767607a5f967de2235e328e50a2949571f71f559
Woongbinni <woongbinni@gmail.com>
2016-04-03 23:56:57 +0900

    boardcover try

---
+++ 001_ALGOSPOT/20160403_BOARDCOVER/boardcover.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+
+    int test_case;
+    char temp;
+
+    scanf("%d", &test_case);
+
+    for(int idx=0; idx<test_case; idx++){
+        int height, width;
+        scanf("%d%d", &height, &width);
+        for(int i=0; i<height; ++i){
+            for(int j=0; j<width; ++j){
+                scanf("%c", &temp);
+                
+
+            }
+        }
+
+    }
+
+    return 0;
+}
