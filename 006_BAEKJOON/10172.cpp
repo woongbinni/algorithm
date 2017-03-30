b3c53297f123e55f7094b1e2e3ec2b42305adc93
woongbinni <bearbin.song@samsung.com>
2017-03-30 17:12:11 +0900

    10172 solved

---
+++ 006_BAEKJOON/10172.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+
+	return 0;
+}
+
