246d19ec46a0985b3ace8eae40b9b0e4e0f9c616
woongbinni <bearbin.song@samsung.com>
2017-04-20 14:22:53 +0900

    2017 05월 사전 solved

--- 007_PRE/2017_05.cpp
+++ 007_PRE/2017_05.cpp
@@ -2,31 +2,24 @@
 using namespace std;
 #include <algorithm>
 #include <vector>
+#include <list>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct _home_info{
-  int idx;
   vector<int> node_list;
 }home_info;
 
 int T, N, M;
-int home_heat[100001];
+long long home_heat[100001];
+char visited[100001];
 home_info home_list[100001];
-
-bool home_cmp(home_info a, home_info b){
-  if(a.node_list.size() != b.node_list.size()){
-    return a.node_list.size() > b.node_list.size();
-  }
-  else{
-    return home_heat[a.idx] > home_heat[b.idx];
-  }
-}
+list<int> bfs_queue;
 
 void print_home_infos(home_info *list){
   for(int i=1; i<=N; ++i){
-    printf("##%d %d %lu\n", list[i].idx, home_heat[list[i].idx], list[i].node_list.size());
+    printf("##%d %lld %lu\n", i, home_heat[i], list[i].node_list.size());
   }
 }
 
@@ -37,17 +30,16 @@ int main(void){
   for(int tc=1; tc<=T; ++tc){
     scanf("%d%d", &N, &M);
 
-    int result = 0;
-    int total_heat = 0;
+    bfs_queue.clear();
+    memset(visited, 0x00, sizeof(visited));
+    long long result = 0;
 
     for(int i=1; i<=N; ++i){
       home_list[i].node_list.clear();
     }
 
     for(int i=1; i<=N; ++i){
-      home_list[i].idx = i;
-      scanf("%d", &(home_heat[i]));
-      total_heat += home_heat[i];
+      scanf("%lld", &(home_heat[i]));
     }
 
     for(int i=1; i<=M; ++i){
@@ -57,33 +49,60 @@ int main(void){
       home_list[t2].node_list.push_back(t1);
     }
 
-    sort(home_list+1, home_list+(N+1), home_cmp);
-
 //    print_home_infos(home_list);
 
-    int idx = 1;
-    while(total_heat > 0){
-      int max_temp = home_heat[home_list[idx].idx];
-
-      if(max_temp != 0){
-
-      home_heat[home_list[idx].idx] = 0;
-      total_heat -= max_temp;
-
-      for(int i=0; i<home_list[idx].node_list.size(); ++i){
-        if(home_heat[home_list[idx].node_list[i]] > max_temp){
-          max_temp = home_heat[home_list[idx].node_list[i]];
-        }
-        total_heat -= home_heat[home_list[idx].node_list[i]];
-        home_heat[home_list[idx].node_list[i]] = 0;
+    for(int i=1; i<=N; ++i){
+      if(home_list[i].node_list.size() == 1){
+        bfs_queue.push_back(i);
       }
+      else if(home_list[i].node_list.size() == 0){
+        result += home_heat[i];
+        home_heat[i] = 0;
+        visited[i] = 1;
       }
-
-      result += max_temp;
-      ++idx;
     }
 
-    printf("#%d %d\n", tc, result);
+    while(bfs_queue.size() > 0){
+      int pop_idx = bfs_queue.front();
+      int parent_idx;
+      bfs_queue.pop_front();
+
+      long long pop_temp = home_heat[pop_idx];
+
+  //    printf("pop : %d %d\n", pop_idx, pop_temp);
+
+      result += home_heat[pop_idx];
+      home_heat[pop_idx] = 0;
+      visited[pop_idx] = 1;
+
+      for(int i=0; i<home_list[pop_idx].node_list.size(); ++i){
+        if(visited[home_list[pop_idx].node_list[i]] == 0){
+          parent_idx = home_list[pop_idx].node_list[i];
+          break;
+        }
+      }
+      home_heat[parent_idx] -= pop_temp;
+      if(home_heat[parent_idx] < 0){
+        home_heat[parent_idx] = 0;
+      }
+
+      int node_cnt = 0;
+      for(int i=0; i<home_list[parent_idx].node_list.size(); ++i){
+        if(visited[home_list[parent_idx].node_list[i]] == 0){
+          node_cnt += 1;
+          home_heat[home_list[parent_idx].node_list[i]] -= pop_temp;
+          if(home_heat[home_list[parent_idx].node_list[i]] < 0)
+            home_heat[home_list[parent_idx].node_list[i]] = 0;
+        }
+      }
+
+      if(node_cnt == 1){
+        bfs_queue.push_back(parent_idx);
+    //    printf("push: %d\n", home_list[pop_idx].node_list[0]);
+      }
+    }
+
+    printf("#%d %lld\n", tc, result);
   }
   return 0;
 }
