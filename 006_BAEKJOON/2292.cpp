b2eb6c85aead388d3e89dc5429fe05424e0337af
bearbin.song <bearbin.song@samsung.com>
2017-07-07 17:02:42 +0900

    2292 solved

--- 006_BAEKJOON/2292.cpp
+++ 006_BAEKJOON/2292.cpp
@@ -5,13 +5,15 @@ using namespace std;
 #include <stdlib.h>
 
 int arr[20001];
+int input;
 
 int main(void){
+
+  scanf("%d", &input);
   arr[0] = 1;
   for(int i=1; i<=20000; ++i){
     arr[i] = arr[i-1] + (6 * (i-1));
-    printf("%d\n", arr[i]);
-    if(arr[i] > 1000000000){
+    if(arr[i] >= input){
       printf("%d\n", i);
       break;
     }
