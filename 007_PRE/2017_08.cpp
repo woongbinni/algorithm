24496a8849d490be216e2103c658bdda2232b6ee
woongbinni <woongbinni@gmail.com>
2017-07-14 21:45:45 +0900

    2017/08 사전 시험 template 작성

---
+++ 007_PRE/2017_08.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int T, N;
+int result;
+
+int main(void){
+    scanf("%d", &T);
+
+    for(int tc=1; tc<=T; ++tc){
+        scanf("%d", &N);
+
+        for(int i=1; i<=N; ++i){
+
+        }
+        
+        printf("#%d %d\n", tc, result);
+    }
+    return 0;
+}
