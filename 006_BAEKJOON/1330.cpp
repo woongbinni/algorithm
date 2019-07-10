aeb9b73f5855f7f6d8be8019f6c01c4fbd821c8a
woongbinni <woongbinni@gmail.com>
2019-07-10 19:56:19 +0900

    1330 solved

---
+++ 006_BAEKJOON/1330.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a < b){
+        printf("<\n");
+    }
+
+    if(a == b){
+        printf("==\n");
+    }
+
+    if(a > b){
+        printf(">\n");
+    }
+
+    return 0;
+}
