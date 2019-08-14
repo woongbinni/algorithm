4d3100122a98ebc9562c1dc5ec283303fde2cf54
woongbinni <woongbinni@gmail.com>
2019-08-14 09:55:28 +0900

    2156 solved

--- 006_BAEKJOON/2156.c
+++ 006_BAEKJOON/2156.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define MAX(a, b) ((a)>(b))?(a):(b)
+#define MAX(a, b) (((a)>(b))?(a):(b))
 
 unsigned long long wine[10001];
-unsigned long long dp[10001];
+unsigned long long dp[10001][3];
 
 int main(){
   int N;
@@ -13,15 +13,20 @@ int main(){
     scanf("%llu", &(wine[i]));
   }
 
-  dp[1] = wine[1];
-  dp[2] = wine[1] + wine[2];
-  dp[3] = MAX(wine[1] + wine[3], wine[2]+wine[3]);
+  dp[1][0] = wine[1];
+  dp[1][1] = wine[1];
+  dp[1][2] = 0;
+  dp[2][0] = dp[1][1] + wine[2];
+  dp[2][1] = wine[2];
+  dp[2][2] = wine[1];
 
-  for(int i=4; i<=N; ++i){
-    dp[i] = MAX(dp[i-3]+wine[i-2]+wine[i], dp[i-3]+wine[i-1]+wine[i]);
+  for(int i=3; i<=N; ++i){
+    dp[i][0] = dp[i-1][1] + wine[i];
+    dp[i][1] = MAX(MAX(dp[i-2][0], dp[i-2][1]), dp[i-2][2]) + wine[i];
+    dp[i][2] = MAX(MAX(dp[i-1][0], dp[i-1][1]), dp[i-1][2]);
   }
 
-  printf("%llu\n", dp[N]);
+  printf("%llu\n", MAX(MAX(dp[N][0], dp[N][1]), dp[N][2]));
 
   return 0;
 }
