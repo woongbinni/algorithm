80c1ec20ac6ae72909e9e727a356f9be2994741a
woongbinni <woongbinni@gmail.com>
2019-08-24 21:08:24 +0900

    11066 solved

--- 006_BAEKJOON/11066.c
+++ 006_BAEKJOON/11066.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int compair(const void *a, const void *b){
-  if(*(int*)a > *(int*)b){
-    return 1;
-  }
-  else if(*(int*)a < *(int*)b){
-    return -1;
-  }
-  else{
-    return 0;
-  }
-}
-
+#define MIN(a, b) ((a)<(b))?(a):(b)
 
 int main(void){
   int T, K;
-  unsigned long long dp[501];
-  unsigned long long file[501];
+  unsigned int dp[501][501];
+  unsigned int file[501];
+  unsigned int sum[501];
 
   scanf("%d", &T);
 
   for(int i=0; i<T; ++i){
     scanf("%d", &K);
-    for(int j=0; j<K; ++j){
-      scanf("%llu", &(file[j]));
-    }
-    qsort(file, K, sizeof(unsigned long long), compair);
+    memset(dp, 0x00, sizeof(dp));
+    memset(file, 0x00, sizeof(file));
+    memset(sum, 0x00, sizeof(sum));
 
-    dp[0] = file[0];
-    dp[1] = file[0] + file[1];
-    for(int j=2; j<K; ++j){
-      dp[j] = dp[j-1] + file[j];
+    for(int j=1; j<=K; ++j){
+      scanf("%d", &(file[j]));
+      sum[j] = sum[j-1]+file[j];
     }
-    printf("%llu\n", dp[K-1]);
+
+    for(int k=1; k<K; ++k){
+      for(int x=1; x + k <=K; ++x){
+        int y = x + k;
+        dp[x][y] = __INT_MAX__;
+
+        for(int mid= x; mid < y; ++mid){
+          dp[x][y] = MIN(dp[x][y], dp[x][mid] + dp[mid+1][y] + sum[y] - sum[x-1]);
+        }
+      }
+    }
+
+    printf("%d\n", dp[1][K]);
   }
   return 0;
 }
